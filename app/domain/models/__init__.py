beb6185fd1c51fc1d71d0aeca3bd3cccccc6dce4
ivan <ibodnar.dev@gmail.com>
2025-10-15 17:03:56 -0300

    add init file for domain models to export entities

---
+++ app/domain/models/__init__.py
@@ -0,0 +1,23 @@
+from app.domain.models.product import (
+    Attribute,
+    AttributeValue,
+    Inventory,
+    Product,
+    ProductStatus,
+    ProductType,
+    ProductVariant,
+    SQLModel,
+    VariantAttributeValue,
+)
+
+__all__ = [
+    "Attribute",
+    "AttributeValue",
+    "Inventory",
+    "Product",
+    "ProductStatus",
+    "ProductType",
+    "ProductVariant",
+    "SQLModel",
+    "VariantAttributeValue",
+]
