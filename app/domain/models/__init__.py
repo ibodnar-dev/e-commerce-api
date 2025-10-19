a105ea2a27641b6bdb5e6e0d27ab0cd4e4234d39
ivan <ibodnar.dev@gmail.com>
2025-10-19 12:42:45 -0300

    remove unused `__all__` declaration from domain models init

--- app/domain/models/__init__.py
+++ app/domain/models/__init__.py
@@ -9,15 +9,3 @@ from app.domain.models.product import (
     SQLModel,
     VariantAttributeValue,
 )
-
-__all__ = [
-    "Attribute",
-    "AttributeValue",
-    "Inventory",
-    "Product",
-    "ProductStatus",
-    "ProductType",
-    "ProductVariant",
-    "SQLModel",
-    "VariantAttributeValue",
-]
