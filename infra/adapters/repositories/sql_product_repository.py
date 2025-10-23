42fb70c1fd94595d53a3c5ded38e331216e467c5
ivan <ibodnar.dev@gmail.com>
2025-10-23 07:50:53 -0300

    remove dependency injection from product repository initialization

--- infra/adapters/repositories/sql_product_repository.py
+++ infra/adapters/repositories/sql_product_repository.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timezone
 from uuid import UUID
 
-from fastapi.params import Depends
 from sqlmodel import Session
 
 from app.domain.ports.repositories import ProductRepository
 from app.domain.models import Product
-from infra.db import get_db_session_generator
 
 
 class SQLProductRepository(ProductRepository):
@@ -23,5 +21,5 @@ class SQLProductRepository(ProductRepository):
         self.session.flush()
 
 
-def get_product_repository(session: Session = Depends(get_db_session_generator)) -> ProductRepository:
+def get_product_repository(session: Session) -> ProductRepository:
     return SQLProductRepository(session)
