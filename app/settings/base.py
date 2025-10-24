96e6e10c1dea9df69c84396a6374900e742e67b5
ivan <ibodnar.dev@gmail.com>
2025-10-24 17:42:31 -0300

    introduce centralized logging configuration and update settings to include customizable log level

--- app/settings/base.py
+++ app/settings/base.py
@@ -7,6 +7,7 @@ from pydantic_settings import BaseSettings
 class SettingsBase(BaseSettings):
     project_root: Path = Field(default=Path(__file__).resolve().parents[2])
     app_db_name: str = "app"
+    log_level: str = "INFO"
 
     @property
     def database_url(self) -> str:
--- app/settings/development.py
+++ app/settings/development.py
@@ -2,6 +2,8 @@ from app.settings.base import SettingsBase
 
 
 class DevSettings(SettingsBase):
+    log_level: str = "DEBUG"
+
     @property
     def database_url(self) -> str:
         return f"sqlite:///{self.project_root}/infra/db/{self.app_db_name}.sqlite3"
