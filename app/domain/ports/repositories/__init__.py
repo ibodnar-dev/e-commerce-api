18e892a62ce6e6d8312929a72c7d021e02e99677
ivan <ibodnar.dev@gmail.com>
2025-10-24 17:41:32 -0300

    add `CounterRepository` to module exports in `__init__.py`

--- app/domain/ports/repositories/__init__.py
+++ app/domain/ports/repositories/__init__.py
@@ -1,3 +1,4 @@
+from app.domain.ports.repositories.counter import CounterRepository
 from app.domain.ports.repositories.product import ProductRepository
 
-__all__ = ["ProductRepository"]
+__all__ = ["CounterRepository", "ProductRepository"]
