c2d81e358b35de18443be40297b142279d40743a
ivan <ibodnar.dev@gmail.com>
2025-10-25 13:57:00 -0300

    remove `async` from `ProductRepository` methods to align with synchronous implementation

--- app/domain/ports/repositories/product.py
+++ app/domain/ports/repositories/product.py
@@ -5,9 +5,9 @@ from app.domain.models.product import Product
 
 class ProductRepository(ABC):
     @abstractmethod
-    async def find_by_id(self, product_id: str) -> Product | None:
+    def find_by_id(self, product_id: str) -> Product | None:
         pass
 
     @abstractmethod
-    async def save(self, product: Product) -> Product:
+    def save(self, product: Product) -> Product:
         pass
