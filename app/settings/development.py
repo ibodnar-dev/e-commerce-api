cb4bc5f9a0b6074c02cafe0f1eb9cfffc017b53a
ivan <ibodnar.dev@gmail.com>
2025-10-30 16:46:31 -0300

    refactor `DevSettings`: extract `database_path` property for improved readability and reuse in `database_url` property

--- app/settings/development.py
+++ app/settings/development.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from app.settings.base import SettingsBase
 
 
@@ -5,8 +7,12 @@ class DevSettings(SettingsBase):
     log_level: str = "DEBUG"
 
     @property
+    def database_path(self) -> Path:
+        return Path(f"sqlite:///{self.project_root}/infra/db/{self.app_db_name}.sqlite3")
+
+    @property
     def database_url(self) -> str:
-        return f"sqlite:///{self.project_root}/infra/db/{self.app_db_name}.sqlite3"
+        return f"sqlite:///{self.database_path}"
 
 
 dev_settings = DevSettings()
