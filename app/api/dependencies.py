8da7e8cb54b5f38f53d27a7a39ec0edfb043af05
ivan <ibodnar.dev@gmail.com>
2025-10-30 16:48:11 -0300

    replace `get_db_session_generator` with `get_managed_db_session` in repository dependencies to reflect updated database connection management

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -4,20 +4,18 @@ from fastapi import Depends
 from sqlmodel import Session
 
 from app.domain.ports.repositories import CounterRepository, ProductRepository
-from infra.adapters.repositories.sql_product_repository import (
-    get_counter_repository,
-    get_product_repository,
-)
-from infra.db.connection import get_db_session_generator
+from infra.adapters.repositories.sql_product import get_product_repository
+from infra.adapters.repositories.sql_system import get_counter_repository
+from infra.db.connection import get_managed_db_session
 
 
 def get_product_repository_dependency(
-    session: Annotated[Session, Depends(get_db_session_generator)],
+    session: Annotated[Session, Depends(get_managed_db_session)],
 ) -> ProductRepository:
     return get_product_repository(session)
 
 
 def get_counter_repository_dependency(
-    session: Annotated[Session, Depends(get_db_session_generator)],
+    session: Annotated[Session, Depends(get_managed_db_session)],
 ) -> CounterRepository:
     return get_counter_repository(session)
