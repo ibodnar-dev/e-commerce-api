7f2901b9a9b47069d9ae31fd57da5fbfa647e98c
ivan <ibodnar.dev@gmail.com>
2025-11-03 08:29:09 -0300

    adjust e2e test fixture: initialize `product_sku_counter` with `current_value=0` and conditionally clean up DB files based on environment

--- tests/e2e/api/conftest.py
+++ tests/e2e/api/conftest.py
@@ -8,7 +8,7 @@ from infra.db.connection import DefaultSession
 @pytest.fixture(scope="session")
 def create_product_counter(setup_db):  # noqa: ARG001
     s = DefaultSession()
-    counter = Counter(name=CounterName.product_sku_counter, current_value=1)
+    counter = Counter(name=CounterName.product_sku_counter, current_value=0)
     s.add(counter)
     s.commit()
     yield counter
--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,9 +1,12 @@
+import os
+
 import pytest
 from fastapi.testclient import TestClient
 
 from app.domain.models import SQLModel
 from app.main import app
 from app.settings import settings
+from app.settings.environments import Environment
 from infra.db import default_engine
 
 
@@ -11,8 +14,9 @@ from infra.db import default_engine
 def setup_db():
     SQLModel.metadata.create_all(default_engine)
     yield
-    for item in settings.database_directory.iterdir():
-        item.unlink()
+    if os.environ.get("APP_ENV") == Environment.e2e.value:
+        for item in settings.db_absolute_path.iterdir():
+            item.unlink()
 
 
 @pytest.fixture(scope="session")
