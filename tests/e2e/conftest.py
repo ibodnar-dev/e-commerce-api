0a3db9e13ae0f5a081dca06091ad0795a2b78f26
ivan <ibodnar.dev@gmail.com>
2025-11-15 07:21:24 -0300

    setup and drop db fns added to setup fixtures

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -4,21 +4,19 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.domain.models import SQLModel
-from app.external.db import default_engine
-from app.external.db.sequences import product_sku_sequence
+from app.external.db import create_tables, default_engine
+from app.external.db.connection import drop_tables
 from app.main import app
 from app.settings.environments import Environment
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def setup_db():
-    # Create all tables before tests
+    create_tables()
     SQLModel.metadata.create_all(default_engine)
     yield
-    # Clean up: drop all tables after tests
     if os.environ.get("APP_ENV") == Environment.e2e.value:
-        SQLModel.metadata.drop_all(default_engine)
-        product_sku_sequence.drop(default_engine, checkfirst=True)
+        drop_tables()
 
 
 @pytest.fixture(scope="session")
--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,17 +3,16 @@ import os
 import pytest
 
 from app.domain.models import SQLModel
-from app.external.db import default_engine
-from app.external.db.sequences import product_sku_sequence
+from app.external.db import create_tables, default_engine
+from app.external.db.connection import drop_tables
 from app.settings import Environment
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_db():
-    # Create all tables before tests
+    create_tables()
     SQLModel.metadata.create_all(default_engine)
     yield
     # Clean up: drop all tables after tests
     if os.environ.get("APP_ENV") == Environment.integration.value:
-        SQLModel.metadata.drop_all(default_engine)
-        product_sku_sequence.drop(default_engine, checkfirst=True)
+        drop_tables()
